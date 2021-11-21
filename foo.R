e2959ffe4965123fd534e4cec2bb77f27e69f6ef
kris e2u <kris.e2u@gmail.com>
2021-11-21 12:40:46 +0100

    Update foo.R
    Finding and fixing bugs in the code

--- foo.R
+++ foo.R
@@ -1,21 +1,42 @@
-# Radius
+# Radius & pi
 r <- 2
+pi <- 3.14159
 
-
-# Function to compute the volume of a sphere with radius r
-volume <- function(r, rho) {
-  3/4*pi*r^2
+# A function to compute the volume of a sphere with radius r=2 
+volume <- function(r) {
+  4/3*pi*r^3
 }
 
+volume(r)
 
-# Function to compute the volumes of the spheres with radius r, r^2 and r^3
-volume_vector <- function(r) {
-  r <- 22
-  for (r in 2:4){
-    volume(r)
+#Now lets see if our function is performing properly (the result should be approx. 33.51):
+4/3*3.14159*2^3
+
+
+# A function to compute the volumes of the spheres with radius r, r^2 and r^3
+
+volume_vector <- function(r){
+  for (i in 1:3) {
+    v <- 4/3*pi
+    r <- 22^3
+    a <- c(1,2,3)
+    result <- v*r^a
+    return(result)
   }
-}
+} 
 
 
 # Run volume_vector(r) and print the volumes of the spheres with radius r, r^2 and r^3
+
 volume_vector(r)
+
+# Again, lets test the function (result should be approx. 44.406, 474.925, and 5.057)
+
+4/3*3.14159*22^3^1
+4/3*3.14159*(22^3)^2
+4/3*3.14159*(22^3)^3
+
+
+#We have now fixed the bugs in the code, and it should run as intended.
+
+
